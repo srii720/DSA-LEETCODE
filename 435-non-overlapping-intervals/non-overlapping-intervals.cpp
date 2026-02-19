4c7bc09c443e512e147180527e23dd95bfe2b55f
srii720 <162484465+srii720@users.noreply.github.com>
2026-02-19 12:46:29 +0530

    Time: 37 ms (85.95%) | Memory: 93.9 MB (65.77%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& arr){
-        if (arr.size() <= 1) return 0;
+        if(arr.empty()) return 0;
 
-        // sort by end time
+        // Sort by ascending end time
         sort(arr.begin(), arr.end(),
-             [](const vector<int>& a, const vector<int>& b) {
-                 return a[1] < b[1];
-             });
+            [](vector<int>& a, vector<int>& b){
+                return a[1] < b[1];
+            });
 
-        int count_to_del = 0;
-        int lastEnd = arr[0][1];
+        int count = 0;
+        int prevend = arr[0][1];
 
-        for (int i = 1; i < arr.size(); i++) {
-            if (arr[i][0] < lastEnd) {
-                // overlap
-                count_to_del++;
+        for(int i = 1; i < arr.size(); i++){
+            if(arr[i][0] < prevend){  // Overlap
+                count++;
             } else {
-                // safe
-                lastEnd = arr[i][1];
+                prevend = arr[i][1];
             }
         }
-
-        return count_to_del;
+        return count;
     }
 };
+///[[1,2],[2,3],[3,4],[1,3]]
+//first element must be smaller then last one
