de005f7ae1b98638d6497e7c856a0c0e58297397
srii720 <162484465+srii720@users.noreply.github.com>
2026-02-20 15:48:12 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (21.50%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        int prev1=0,prev2=0;
-        for(int i=0;i<nums.size();i++){
-            int take=nums[i]+prev2;
-            int skip=prev1;
-            int current=max(skip,take);
-            prev2=prev1;
-            prev1=current;
-            
-        }return prev1;
-       
+   int rob(std::vector<int>&nums){
+    std::vector<int>dp(nums.size(),-1);
+    return rob_helper(nums,0,dp);
+}
+int rob_helper(std::vector<int>&nums,int i,std::vector<int>&dp){
+    if(i>=nums.size())return 0;
+    if(dp[i]!=-1)return dp[i];
+    int take=nums[i]+rob_helper(nums,i+2,dp);
+    int give=rob_helper(nums,i+1,dp);
+    return dp[i]=std::max(take,give);
 
 
-    }
 
+}
 };
