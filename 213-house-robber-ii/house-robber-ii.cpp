34937be278b3ca66a1b12405755aba854833d42d
Srijan Ghimire <162484465+srii720@users.noreply.github.com>
2026-02-23 22:13:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (96.96%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     // Linear robber for range [l, r]
     int robRange(vector<int>& nums, int l, int r) {
-        int prev2 = 0;   // dp[i-2]
-        int prev1 = 0;   // dp[i-1]
+        int prev2 = 0;
+        int prev1 = 0;
 
-        for (int i = l; i <= r; ++i) {
-            int pick = nums[i] + prev2;
+        for (int i = l; i <= r; i++) {
+            int take = nums[i] + prev2;
             int skip = prev1;
-            int cur = max(pick, skip);
+            int current = max(take, skip);
 
             prev2 = prev1;
-            prev1 = cur;
+            prev1 = current;
         }
+
         return prev1;
     }
 
@@ -20,14 +21,11 @@ public:
         int n = nums.size();
 
         if (n == 0) return 0;
-        if (n == 1) return nums[0];   // Important edge case
+        if (n == 1) return nums[0];
 
-        // Case 1: Exclude last house
-        int takeFirst = robRange(nums, 0, n - 2);
+        int case1 = robRange(nums, 0, n - 2);
+        int case2 = robRange(nums, 1, n - 1);
 
-        // Case 2: Exclude first house
-        int skipFirst = robRange(nums, 1, n - 1);
-
-        return max(takeFirst, skipFirst);
+        return max(case1, case2);
     }
 };
