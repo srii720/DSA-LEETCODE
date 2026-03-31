876e715d56037ab7d62359f5f8e2fcef9ebc792e
Srijan Ghimire <162484465+srii720@users.noreply.github.com>
2026-03-31 15:29:05 +0530

    Time: 1092 ms (5.03%) | Memory: 23.9 MB (19.46%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+       int start=0;
+        for(int j=0;j<nums.size();j++){
+        for(int i=0;i+1<nums.size();i++){
+            if(nums[i]==0&&nums[i+1]!=0){
+             swap(nums[i],nums[i+1]);
+            }else{
+                start++;
+            }
+        }}
+       
+    }
+};
