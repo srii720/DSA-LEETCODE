65d0f3d172e448d1e7d556c4b33c4f618e581e81
Srijan Ghimire <162484465+srii720@users.noreply.github.com>
2026-03-31 13:51:28 +0530

    Time: 0 ms (100.00%) | Memory: 97.5 MB (24.20%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -3,7 +3,7 @@ public:
     int maxProfit(std::vector<int>& prices) {
         int min_price=prices[0];
         int max_profit=0;
-        for(int i=1;i<prices.size();i++){
+        for(int i=0;i<prices.size();i++){
             int profit_today=prices[i]-min_price;
             max_profit=max(max_profit,profit_today);
             min_price=min(min_price,prices[i]);
